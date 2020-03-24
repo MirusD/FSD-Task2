b9454bbf4f16b64182f416e445e3fa41a76f6ce2
MirusD <damir.salihov@inbox.ru>
2020-03-24 21:13:26 +0500

    create template room-card

---
+++ src/components/room-card/room-card.js
@@ -0,0 +1,13 @@
+import '../../../node_modules/slick-carousel/slick/slick.css'
+// import '../../../node_modules/slick-carousel/slick/slick-theme.css'
+import 'slick-carousel'
+import './room-card.scss'
+
+$('.room-card__slider').slick({
+    dots: true
+});
+
+// $('.room-card__slider.arrows-off').slick({
+//     dots: true,
+//     arrows: true
+// });
