07ded155bf46ce7a8be274bf5584006d0a97b381
MirusD <damir.salihov@inbox.ru>
2020-05-01 01:51:07 +0500

    fix date dropdown, rabge slider, like button

--- src/components/like-button/like-button.js
+++ src/components/like-button/like-button.js
@@ -1,1 +1,25 @@
 import './like-button.scss'
+
+class LikeButton {
+    constructor (component) {
+        this.component = component;
+        this.numberLikes = this.component.querySelector('.like-button__number-likes');
+        this.likes = +this.numberLikes.innerHTML;
+        this.init();
+    }
+    init () {
+        this.component.addEventListener('click', () => {
+            if (this.component.classList.contains('clicked'))
+                this.likes -= 1;
+            else
+                this.likes += 1;
+
+            this.component.classList.toggle('clicked');
+            this.numberLikes.innerHTML = this.likes;
+        })
+    }
+}
+
+$('.like-button').each((index, node) => {
+    new LikeButton(node);
+});
--- src/components/range-slider/range-slider.js
+++ src/components/range-slider/range-slider.js
@@ -2,6 +2,30 @@ const noUiSlider = require('nouislider');
 import '../../../node_modules/nouislider/distribute/nouislider.min.css'
 import './range-slider.scss'
 
+class RangeSlider {
+    constructor (component) {
+        this.component = component;
+        this.init();
+    }
+    init () {
+        noUiSlider.create(this.component, {
+            start: [5000, 10000],
+            step: 100,
+            behaviour: 'drag',
+            connect: true,
+            range: {
+                'min': 0,
+                'max': 14900
+        }
+});
+
+    }
+}
+
+$('.range-slider__linear').each((index, node) => {
+   new RangeSlider(node);
+});
+
 // var rangeSlider = document.querySelector('.range-slider__linear');
 // var nodes = [
 //     document.querySelector('.range-slider__from'),
