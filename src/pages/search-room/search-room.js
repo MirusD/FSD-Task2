e9c05ace26cb982f81f02fabf2d6232e66cbd56c
MirusD <damir.salihov@inbox.ru>
2020-07-01 02:13:53 +0500

    filter open closed

--- src/pages/search-room/search-room.js
+++ src/pages/search-room/search-room.js
@@ -1,1 +1,19 @@
 import './search-room.scss'
+
+let filter = document.querySelector('.filter');
+let screenBlack = document.querySelector('.filter__screen-black');
+let btnOpenFilter = document.querySelector('.search-room__btn-open-filter');
+let btnClosed = document.querySelector('.filter__btn-closed');
+let doc = document.querySelector('body');
+
+btnOpenFilter.addEventListener('click', () => {
+    filter.classList.add('opened');
+    screenBlack.classList.add('active');
+    doc.style.overflow="hidden";
+});
+
+btnClosed.addEventListener('click', () => {
+    filter.classList.remove('opened');
+    screenBlack.classList.remove('active');
+    doc.style.overflow="auto";
+});
